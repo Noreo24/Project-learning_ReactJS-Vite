f1d7c02e7c15e3affbfe14d5d9b3247a67574787
Tuấn Bùi Anh <buianhtuan.kbhb24@gmail.com>
2025-10-02 00:54:30 +0700

    Practice with components - 00h54 02/10/2025

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,15 @@
+
+const TodoData = () => {
+    return (
+        <div>
+            <div className="todo-data">
+                <ul>
+                    <li className="todo-item">Learning ReactJS</li>
+                    <li className="todo-item">Learning NodeJS</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default TodoData;
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,11 @@
+
+const TodoNew = () => {
+    return (
+        <div className='todo-new'>
+            <input className='todo-input' type="text" placeholder='Add a new task' />
+            <button className="add-button">Add</button>
+        </div>
+    );
+}
+
+export default TodoNew;
