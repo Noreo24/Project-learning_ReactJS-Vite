342448e1b842705a35f3fee99844aadbc1671ded
Tuấn Bùi Anh <buianhtuan.kbhb24@gmail.com>
2025-10-21 23:36:35 +0700

    Nested route with Outlet - 23h36 21/10/2025

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import reactLogo from './assets/react.svg';
 import { useState } from 'react';
 import Header from './components/layouts/header';
 import Footer from './components/layouts/footer';
+import { Outlet } from 'react-router-dom';
 
 // Sử dụng component bên trong component khác
 // Component cha (App) sử dụng component con (MyComponent)
@@ -79,6 +80,7 @@ const App = () => {
         }
         {/* Tương tự dùng if...else trong java = ? : */}
       </div>
+      <Outlet />
       <Footer />
     </>
   )
