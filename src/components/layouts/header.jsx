b04748c0a355bea485266571934aa7607723d1f8
Tuấn Bùi Anh <buianhtuan.kbhb24@gmail.com>
2025-10-21 23:45:30 +0700

    Avoid reloading page with <Link></Link> - 23h45 21/10/2025

--- src/components/layouts/header.jsx
+++ src/components/layouts/header.jsx
@@ -1,12 +1,15 @@
+import { Link } from 'react-router-dom'
 import './header.css'
 const Header = () => {
     return (
         <ul className="header">
-            <li className="header-item"><a className="active" href="/">Home</a></li>
-            <li className="header-item"><a href="/users">Users</a></li>
-            <li className="header-item"><a href="/products">Products</a></li>
+            <li className="header-item"><Link className="active" to="/">Home</Link></li>
+            <li className="header-item"><Link to="/users">Users</Link></li>
+            <li className="header-item"><Link to="/products">Products</Link></li>
         </ul>
     )
+    // Dùng thẻ <a></a> để tạo link chuyển trang nhưng sẽ load lại toàn bộ trang
+    // Cách tốt hơn là dùng Link hoặc NavLink của react-router-dom
 }
 
 export default Header
