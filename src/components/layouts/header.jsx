bf872241be09ca55a6e9c84fd423c0e1b38dbd6f
Tuấn Bùi Anh <buianhtuan.kbhb24@gmail.com>
2025-10-21 23:50:07 +0700

    Use <NavLink></NavLink> to add the active attribute - 23h49 21/10/2025

--- src/components/layouts/header.jsx
+++ src/components/layouts/header.jsx
@@ -1,15 +1,16 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './header.css'
 const Header = () => {
     return (
         <ul className="header">
-            <li className="header-item"><Link className="active" to="/">Home</Link></li>
-            <li className="header-item"><Link to="/users">Users</Link></li>
-            <li className="header-item"><Link to="/products">Products</Link></li>
+            <li className="header-item"><NavLink to="/">Home</NavLink></li>
+            <li className="header-item"><NavLink to="/users">Users</NavLink></li>
+            <li className="header-item"><NavLink to="/products">Products</NavLink></li>
         </ul>
     )
     // Dùng thẻ <a></a> để tạo link chuyển trang nhưng sẽ load lại toàn bộ trang
     // Cách tốt hơn là dùng Link hoặc NavLink của react-router-dom
+    // Dùng NavLink thay vì Link để có thể thêm class active khi trang đang ở đường dẫn đó
 }
 
 export default Header
