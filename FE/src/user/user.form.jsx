f9131748204aad8154aa3487d14347fef4eaa6c6
Tuấn Bùi Anh <buianhtuan.kbhb24@gmail.com>
2025-11-09 16:39:20 +0700

    Complete user form - 16h39 09/11/2025

--- FE/src/user/user.form.jsx
+++ FE/src/user/user.form.jsx
@@ -1,27 +1,46 @@
 import { Button, Input } from "antd";
+import { useState } from "react";
 
 const UserForm = () => {
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const handleSubmit = () => {
+        console.log({ fullName, email, password, phone });
+    };
     return (
         <div className="user-form">
             <div>
                 <div>
                     <span>Fullname</span>
-                    <Input />
+                    <Input
+                        value={fullName}
+                        onChange={(event) => { setFullName(event.target.value) }} />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input />
+                    <Input
+                        value={email}
+                        onChange={(event) => { setEmail(event.target.value) }} />
                 </div>
                 <div>
                     <span>Password</span>
-                    <Input.Password />
+                    <Input.Password
+                        value={password}
+                        onChange={(event) => { setPassword(event.target.value) }} />
                 </div>
                 <div>
                     <span>Phone</span>
-                    <Input />
+                    <Input
+                        value={phone}
+                        onChange={(event) => { setPhone(event.target.value) }} />
                 </div>
                 <div>
-                    <Button type="primary">Submit</Button>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSubmit()}>Submit</Button>
                 </div>
             </div>
         </div>
